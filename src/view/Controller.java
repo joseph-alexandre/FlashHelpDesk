88f0b2b692a2df9411c78b33538538fafb31e5fc
Joseph Alexandre <j.alexandremmfreitas@gmail.com>
2019-06-05 14:47:40 -0300

    Adicionando JavaFX no projeto

--- src/view/Controller.java
+++ src/view/Controller.java
@@ -1,21 +1,4 @@
 package view;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class Controller implements Initializable {
-
-    @FXML
-    private void acaoDoBotao(ActionEvent event){
-        System.out.println("You clicked me!");
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
-    }
+public class Controller {
 }
